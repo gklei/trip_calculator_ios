c4de12a7c6d8b957b1656414a703963764a487c5
Gregory Klein <gregory@incipia.co>
2020-12-03 11:54:05 -0500

    Initial Commit

---
+++ trip-calculator/trip_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  trip_calculatorApp.swift
+//  trip-calculator
+//
+//  Created by Gregory Klein on 12/3/20.
+//
+
+import SwiftUI
+
+@main
+struct trip_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
