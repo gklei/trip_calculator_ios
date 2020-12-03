8f980895856fffd23bb4929c82bab14c073f4cae
Gregory Klein <gregory@incipia.co>
2020-12-03 12:18:45 -0500

    Initial commit with API models setup.

---
+++ trip-calculator/API.swift
@@ -0,0 +1,92 @@
+//
+//  API.swift
+//  trip-calculator
+//
+//  Created by Gregory Klein on 12/3/20.
+//
+
+import Combine
+import Foundation
+
+// MARK: - Student
+struct Student: Codable, Identifiable {
+   let id: Int
+   let name: String
+   let totalExpenseAmount: Float
+   let expenseItems: [ExpenseItem]
+   
+   enum CodingKeys: String, CodingKey {
+      case id, name
+      case totalExpenseAmount = "total_expense_amount"
+      case expenseItems = "expense_items"
+   }
+}
+
+// MARK: - StudentList
+struct StudentList: Codable {
+   let students: [Student]
+}
+
+// MARK: - ExpenseItem
+struct ExpenseItem: Codable {
+   let id: Int
+   let name: String
+   let studentID: Int
+   let amount: Float
+   let studentName: String
+   
+   enum CodingKeys: String, CodingKey {
+      case id, name, amount
+      case studentID = "student_id"
+      case studentName = "student_name"
+   }
+}
+
+// MARK: - ExpenseItemList
+struct ExpenseItemList: Codable {
+   let totalAmount: Float
+   let expenseItems: [ExpenseItem]
+   
+   enum CodingKeys: String, CodingKey {
+      case totalAmount = "total_amount"
+      case expenseItems = "expense_items"
+   }
+}
+
+struct Agent {
+   struct Response<T> {
+      let value: T
+      let response: URLResponse
+   }
+   
+   func run<T: Decodable>(_ request: URLRequest) -> AnyPublisher<Response<T>, Error> {
+      return URLSession.shared
+         .dataTaskPublisher(for: request)
+         .tryMap { result -> Response<T> in
+            let value = try JSONDecoder().decode(T.self, from: result.data)
+            return Response(value: value, response: result.response)
+         }
+         .receive(on: DispatchQueue.main)
+         .eraseToAnyPublisher()
+   }
+}
+
+enum API {
+   static let agent = Agent()
+   static let base = URL(string: "http://127.0.0.1:5000/")!
+}
+
+extension API {
+   static func getStudents() -> AnyPublisher<StudentList, Error> {
+      var request = URLRequest(
+         url: base.appendingPathComponent("students"),
+         cachePolicy: .useProtocolCachePolicy,
+         timeoutInterval: 10.0
+      )
+      request.httpMethod = "GET"
+      request.allHTTPHeaderFields = ["Content-Type": "application/json"]
+      return agent.run(request)
+         .map(\.value)
+         .eraseToAnyPublisher()
+   }
+}
--- trip-calculator/ContentView.swift
+++ trip-calculator/ContentView.swift
@@ -6,16 +6,43 @@
 //
 
 import SwiftUI
+import Combine
+
+class ViewModel: ObservableObject {
+   @Published var studentList: StudentList = StudentList(students: [])
+   private var disposables = Set<AnyCancellable>()
+   
+   func getStudents() {
+      API.getStudents()
+         .sink { (_) in
+         } receiveValue: { (list) in
+            self.studentList = list
+         }
+         .store(in: &disposables)
+   }
+}
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+   @State var disposables = Set<AnyCancellable>()
+   @StateObject var viewModel = ViewModel()
+   
+   var body: some View {
+      NavigationView {
+         List {
+            ForEach(viewModel.studentList.students) { student in
+               Text(student.name)
+            }
+         }
+         .navigationBarTitle("Trip Calculator")
+      }
+      .onAppear(perform: {
+         viewModel.getStudents()
+      })
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   static var previews: some View {
+      ContentView()
+   }
 }
