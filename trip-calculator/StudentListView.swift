d7d6f864ffc23da83fa3259acf840d86fe6476a6
Gregory Klein <gregory@incipia.co>
2020-12-05 15:39:55 -0500

    Adjusted the StudentListView view model to be a nested class within the StudentListView.

--- trip-calculator/StudentListView.swift
+++ trip-calculator/StudentListView.swift
@@ -8,43 +8,6 @@
 import SwiftUI
 import Combine
 
-class ViewModel: ObservableObject {
-   @Published var students: [Student] = []
-   private var disposables = Set<AnyCancellable>()
-   
-   func getStudents() {
-      API.getStudents()
-         .sink { (_) in
-         } receiveValue: { (list) in
-            self.students = list.students
-         }
-         .store(in: &disposables)
-   }
-   
-   func onAdd(name: String) {
-      API.createStudent(name: name)
-         .sink { (_) in
-         } receiveValue: { _ in
-            self.getStudents()
-         }
-         .store(in: &disposables)
-   }
-   
-   func onDelete(offsets: IndexSet) {
-      guard offsets.count == 1 else { return }
-      let offset = offsets.first!
-      withAnimation {
-         let student = students[offset]
-         API.deleteStudent(id: student.id)
-            .sink { (_) in
-            } receiveValue: { (list) in
-               self.students = list.students
-            }
-            .store(in: &disposables)
-      }
-   }
-}
-
 struct StudentListView: View {
    @State var disposables = Set<AnyCancellable>()
    @StateObject var viewModel = ViewModel()
@@ -88,6 +51,45 @@ struct StudentListView: View {
             viewModel.onAdd(name: name)
          })
       )
+   }
+}
+
+extension StudentListView {
+   class ViewModel: ObservableObject {
+      @Published var students: [Student] = []
+      private var disposables = Set<AnyCancellable>()
+      
+      func getStudents() {
+         API.getStudents()
+            .sink { (_) in
+            } receiveValue: { (list) in
+               self.students = list.students
+            }
+            .store(in: &disposables)
+      }
+      
+      func onAdd(name: String) {
+         API.createStudent(name: name)
+            .sink { (_) in
+            } receiveValue: { _ in
+               self.getStudents()
+            }
+            .store(in: &disposables)
+      }
+      
+      func onDelete(offsets: IndexSet) {
+         guard offsets.count == 1 else { return }
+         let offset = offsets.first!
+         withAnimation {
+            let student = students[offset]
+            API.deleteStudent(id: student.id)
+               .sink { (_) in
+               } receiveValue: { (list) in
+                  self.students = list.students
+               }
+               .store(in: &disposables)
+         }
+      }
    }
 }
 
